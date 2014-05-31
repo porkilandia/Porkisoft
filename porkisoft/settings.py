950f391603c1fc27ab6a61b39f7ad964e19bf56e
jamen17 <jamen17@gmail.com>
2014-05-30 19:27:51 -0500

    ahora la BD

--- porkisoft/settings.py
+++ porkisoft/settings.py
@@ -160,3 +160,9 @@ LOGGING = {
         },
     }
 }
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER =('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
