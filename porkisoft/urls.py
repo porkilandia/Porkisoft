ebcebbed5645cab80a1db4ba63b65f185f2bc250
jamen17 <jamen17@gmail.com>
2015-01-30 14:25:39 -0500

    Copia de seguridad 29 Enero 2015

--- porkisoft/urls.py
+++ porkisoft/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from Inventario.views import inicio
+from Inventario.views import inicio,home
 from  Ventas.views import PuntoVenta
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$',inicio),
+    url(r'^infoGral/$',home),
     url(r'^VentasNorte/',PuntoVenta),
 
     url(r'^inventario/', include('Inventario.urls')),
