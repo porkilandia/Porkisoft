bcca67d33e3691b20ad7773fe752a4ae587f32fe
jamen17 <jamen17@gmail.com>
2015-02-27 16:27:06 -0500

    SE CAMBIAN VALORES DE DESPOSTE

--- Fabricacion/views.py
+++ Fabricacion/views.py
@@ -1457,8 +1457,8 @@ def GestionDesposteActualizado(request, idplanilla):
         pesoAsumido =Decimal(vrDesecho) + perdidaPeso
         vrCarnes =Decimal(vrCarnes) + pesoAsumido
     else:
-        vrCarnes = ceil((vrTotalCanales * Decimal(8))/100)
-        vrCarnes2 = ceil((vrTotalCanales * Decimal(32))/100)
+        vrCarnes = ceil((vrTotalCanales * Decimal(10))/100)
+        vrCarnes2 = ceil((vrTotalCanales * Decimal(30))/100)
         vrCarnes3 = ceil((vrTotalCanales * Decimal(31.5))/100)
         vrCarnes4 = ceil((vrTotalCanales * Decimal(7))/100)
         vrCostillas = ceil((vrTotalCanales * 4)/100)
