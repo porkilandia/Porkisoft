c118765330e9c0bfc68f403b23b102c87f1287b9
jamen17 <jamen17@gmail.com>
2015-04-24 11:28:24 -0500

    Adicionando funcionalidad a Reporte Faltantes

--- porkisoft/templates/static/js/acciones.js
+++ porkisoft/templates/static/js/acciones.js
@@ -1402,8 +1402,24 @@ function Exportar()
 }
 function conciliarFaltantes() {
 
-    var tablaReporteFaltante = $('#tablaReporteFaltante');
-
+    $("#tabla tbody tr").each(function (index)
+        {
+            var campo1, campo2, campo3;
+            $(this).children("td").each(function (index2)
+            {
+                switch (index2)
+                {
+                    case 0: campo1 = $(this).text();
+                            break;
+                    case 1: campo2 = $(this).text();
+                            break;
+                    case 2: campo3 = $(this).text();
+                            break;
+                }
+                $(this).css("background-color", "#ECF8E0");
+            });
+            alert(campo1 + ' - ' + campo2 + ' - ' + campo3);
+        });
 }
 $('#tablaReporteFaltante').delegate('tr.pesos','change', function(){
     var fisico = $(".actual",this).val();
