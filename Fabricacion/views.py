254d286b42d859ab8825771f0729745263b33fee
jamen17 <jamen17@gmail.com>
2015-07-29 11:01:26 -0500

    cambios en compras

--- Fabricacion/views.py
+++ Fabricacion/views.py
@@ -14,7 +14,7 @@ from Fabricacion.Forms import *
 from Fabricacion.models import *
 from Inventario.models import *
 from Ventas.models import *
-#comentario
+#comentario 2
 def configuracionDesposteTemplate(request):
     grupos = Grupo.objects.select_related()
 
